f00699a84b37bd6c558b9bf082933170bc471afd
Ritu-1120 <rushitajoshi1120@gmail.com>
2024-08-20 19:52:48 +0100

    Update data_transformer.py

--- data_transformer.py
+++ data_transformer.py
@@ -1,1 +1,11 @@
+import pandas as pd
 
+def transform_data(raw_data):
+    df = pd.DataFrame(raw_data)
+    df['transformed_column'] = df['some_column'].apply(lambda x: x * 2)  # Example transformation
+    return df
+
+if __name__ == "__main__":
+    raw_data = [{'some_column': 1}, {'some_column': 2}]
+    transformed_data = transform_data(raw_data)
+    print(transformed_data)
